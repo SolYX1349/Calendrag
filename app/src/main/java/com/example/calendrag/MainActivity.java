75e0a105bb69e4c8a3c03559994df60b84dd4331
SoLYX1349 <saul77023@gmail.com>
2024-06-23 17:06:12 -0600

    Se agrego la validacion del loguin

--- app/src/main/java/com/example/calendrag/MainActivity.java
+++ app/src/main/java/com/example/calendrag/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.calendrag;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String DEFAULT_EMAIL = "12345678@utfv.edu.mx";
+    private static final String DEFAULT_PASSWORD = "1234567890";
+
+    private EditText edtEmail;
+    private EditText edtPassword;
+
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,51 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void send(View view) {
+        getInformation();
+
+        if (isValid()) {
+            //Resend to the next sheet
+            Toast.makeText(this, "Exito al iniciar.", Toast.LENGTH_LONG).show();
+        } else {
+            //Send an error message
+            Toast.makeText(this, "Error al iniciar sesion intenta de nuevo.", Toast.LENGTH_LONG).show();
+        }
+        clean();
+    }
+
+    private void assignInformation() {
+        this.email = this.edtEmail.getText().toString();
+        this.password = this.edtPassword.getText().toString();
+    }
+
+    private void getInformation() {
+        this.edtEmail = findViewById(R.id.editTextTextEmailAddress);
+        this.edtPassword = findViewById(R.id.editTextTextPassword);
+        assignInformation();
+    }
+
+    private boolean isValid() {
+        return this.email.equals(DEFAULT_EMAIL) && this.password.equals(DEFAULT_PASSWORD);
+    }
+
+    private void clean() {
+        resetDisplay();
+        resetVariables();
+    }
+
+    private void resetDisplay() {
+        this.edtEmail.setText("");
+        this.edtPassword.setText("");
+    }
+
+    private void resetVariables() {
+        this.edtEmail = null;
+        this.edtPassword = null;
+
+        this.email = null;
+        this.password = null;
     }
 }
