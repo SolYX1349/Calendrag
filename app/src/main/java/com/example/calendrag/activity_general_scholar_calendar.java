e60e87bd4f7834a7dee3b5db78e0bcf2b0338856
SoLYX1349 <saul77023@gmail.com>
2024-06-29 09:09:44 -0600

    se agregro el disenio y la funcionalidad de envio al act calendario

--- app/src/main/java/com/example/calendrag/activity_general_scholar_calendar.java
+++ app/src/main/java/com/example/calendrag/activity_general_scholar_calendar.java
@@ -1,8 +1,6 @@
 package com.example.calendrag;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,21 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class activity_home_guest extends AppCompatActivity {
+public class activity_general_scholar_calendar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home_guest);
+        setContentView(R.layout.activity_general_scholar_calendar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-    public void goToCalendar(View view){
-        Intent intent = new Intent(this, activity_general_scholar_calendar.class);
-        startActivity(intent);
     }
 }
